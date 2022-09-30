56795bf6c7aefdbbbf18a51089bfba749eb51826
Atadurdy Mashadov <113720165+AtashkaMashadov@users.noreply.github.com>
2022-10-01 00:21:37 +0500

    Add files via upload

---
+++ Big big menu.py
@@ -0,0 +1,339 @@
+print("Hello customers!")
+print("Dont forget to buy our firmenal pizzas!")
+a=input("would you like to eat?yes or no?")
+if a=="YES" or a=="yes" or a=="Yes" or a=="yES" or a=="yeS" or a=="YEs" or a=="yEs" or a=="YeS":
+    print("That is good!")
+    b=input("""what do you want to eat?
+    We got a big big menu!
+    Pizza
+    Salad
+    pasta
+    Turkmen food
+    You can choose everything you want  """)
+    if b=="pizza" or b=="Pizza" or b=="PIZZA":
+        print("what kind of pizza do you want to eat?")
+        c=input("""  multi pizza 
+            pizza with meat 
+            pizza without meat
+            simple pizza
+            veggie pizza""")
+        if c=="multi pizza":
+            d=input("do you want to buy 2 pizza!(yes/no)")
+            if d=="YES" or d=="yes" or d=="Yes" or d=="yES" or d=="yeS" or d=="YEs" or d=="yEs" or d=="YeS":
+                e=input("""  multi pizza 
+                        pizza with meat 
+                        pizza without meat
+                        simple pizza
+                        veggie pizza""")
+                if e=="multi pizza":
+                    print("you have to pay 50$")
+                elif e=="veggie pizza":
+                    print("you have to pay 89.5$")
+                elif e=="simple pizza":
+                    print("you have to pay 60$")
+                elif e=="pizza with out meat":
+                    print("you have to pay 40.5$")
+                elif e == "pizza with meat":
+                    print("you have to pay 48.5$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("OK 30$.please")
+        elif c == "simple pizza":
+            d = input("do you want to buy 2 pizza!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""  multi pizza 
+                               pizza with meat 
+                               pizza without meat
+                               simple pizza
+                               veggie pizza""")
+                if e == "multi pizza":
+                    print("you have to pay 50$")
+                elif e == "veggie pizza":
+                    print("you have to pay 89.5$")
+                elif e == "simple pizza":
+                    print("you have to pay 60$")
+                elif e == "pizza with out meat":
+                    print("you have to pay 40.5$")
+                elif e == "pizza with meat":
+                    print("you have to pay 48.5$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("Sizden 25$!")
+        elif c == "veggie pizza":
+            d = input("do you want to buy 2 pizza!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""  multi pizza 
+                               pizza with meat 
+                               pizza without meat
+                               simple pizza
+                               veggie pizza""")
+                if e == "multi pizza":
+                    print("you have to pay 50$")
+                elif e == "veggie pizza":
+                    print("you have to pay 89.5$")
+                elif e == "simple pizza":
+                    print("you have to pay 60$")
+                elif e == "pizza with out meat":
+                    print("you have to pay 40.5$")
+                elif e == "pizza with meat":
+                    print("you have to pay 48.5$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("Sizden 25$!")
+        elif c == "pizza with out meat":
+            d = input("do you want to buy 2 pizza!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""  multi pizza 
+                               pizza with meat 
+                               pizza without meat
+                               simple pizza
+                               veggie pizza""")
+                if e == "multi pizza":
+                    print("you have to pay 50$")
+                elif e == "veggie pizza":
+                    print("you have to pay 89.5$")
+                elif e == "simple pizza":
+                    print("you have to pay 60$")
+                elif e == "pizza with out meat":
+                    print("you have to pay 40.5$")
+                elif e == "pizza with meat":
+                    print("you have to pay 48.5$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("Sizden 25$!")
+        elif c == "pizza with meat":
+            d = input("do you want to buy 2 pizza!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""  multi pizza 
+                                      pizza with meat 
+                                      pizza without meat
+                                      simple pizza
+                                      veggie pizza""")
+                if e == "multi pizza":
+                    print("you have to pay 50$")
+                elif e == "veggie pizza":
+                    print("you have to pay 89.5$")
+                elif e == "simple pizza":
+                    print("you have to pay 60$")
+                elif e == "pizza with out meat":
+                    print("you have to pay 40.5$")
+                elif e == "pizza with meat":
+                    print("you have to pay 48.5$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("Sizden 25$!")
+        else:
+            print("tazeden yazyn!")
+
+#Salad
+    elif b=="salad" or b=="Salad" or b=="SALAD":
+        print("what kind of Salad do you want to eat?")
+        c=input("""Sezar 
+        Prinz
+        Simple salad""")
+        if c=="Sezar" or c=="sezar" or c=="SEZAR":
+            d = input("do you want to buy 2 Salad!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""Sezar 
+                    Prinz
+                       Simple salad""")
+                if c == "Sezar" or c == "sezar" or c == "SEZAR":
+                    print("you have to pay 25$")
+                elif c == "Prinz" or c == "prinz" or c == "PRINZ":
+                    print("you have to pay 20$")
+                elif c == "Simple salad" or c == "simple salad" or c == "SIMPLE SALAD":
+                    print("you have to pay 30$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("you have to pay 15")
+
+        elif c=="Prinz" or c=="prinz" or c=="PRINZ":
+            d = input("do you want to buy 2 Salad!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""Sezar 
+                                   Prinz
+                                   Simple salad""")
+                if c == "Sezar" or c == "sezar" or c == "SEZAR":
+                    print("you have to pay 25$")
+                elif c == "Prinz" or c == "prinz" or c == "PRINZ":
+                    print("you have to pay 20$")
+                elif c == "Simple salad" or c == "simple salad" or c == "SIMPLE SALAD":
+                    print("you have to pay 30$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("Sizden 40$")
+        elif c=="Simple salad" or c=="simple salad" or c=="SIMPLE SALAD":
+            d = input("do you want to buy 2 Salad!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""Sezar 
+                                   Prinz
+                                   Simple salad""")
+                if c == "Sezar" or c == "sezar" or c == "SEZAR":
+                    print("you have to pay 25$")
+                elif c == "Prinz" or c == "prinz" or c == "PRINZ":
+                    print("you have to pay 20$")
+                elif c == "Simple salad" or c == "simple salad" or c == "SIMPLE SALAD":
+                    print("you have to pay 30$")
+                else:
+                    print("tazeden yazyn!")
+            else:
+                print("Sizden 40$")
+        else:
+            print("tazeden yazyn!")
+#3 pasta
+    elif b == "pasta" or b == "Pasta" or b == "PASTA":
+        print("what kind of pasta do you want to eat?")
+        c = input("""
+        Spagetti
+        macaroni
+        noodles""")
+        if c == "spagetti":
+            d = input("do you want to buy 2 pasta!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""Spagetti
+                macaroni
+                noodles""")
+                if e == "spagetti":
+                    print("you have to pay 50$")
+                elif e == "macaroni":
+                    print("you have to pay 89.5$")
+                elif e == "noodles":
+                    print("you have to pay 60$")
+                else:
+                    print("tazeden yazyn")
+            else:
+                print("Sizden 40$")
+        elif c == "macaroni":
+            d = input("do you want to buy 2 pasta!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""Spagetti
+                macaroni
+                noodles""")
+                if e == "spagetti":
+                    print("you have to pay 50$")
+                elif e == "macaroni":
+                    print("you have to pay 89.5$")
+                elif e == "noodles":
+                    print("you have to pay 60$")
+                else:
+                    print("tazeden yazyn")
+            else:
+                print("Sizden 40$")
+        elif c == "noodles":
+            d = input("do you want to buy 2 pasta!(yes/no)")
+            if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                e = input("""Spagetti
+                macaroni
+                noodles""")
+                if e == "spagetti":
+                    print("you have to pay 50$")
+                elif e == "macaroni":
+                    print("you have to pay 89.5$")
+                elif e == "noodles":
+                    print("you have to pay 60$")
+                else:
+                    print("tazeden yazyn")
+            else:
+                print("Sizden 40$")
+        else:
+            print("tazeden yazyn!")
+
+    elif b == "Turkmen food" or b == "turkmen food" or b == "TURKMEN FOOD":
+            print("what kind of Turkmen food do you want to eat?")
+            c = input("""
+            Palow
+            dolama
+            manty
+            tayborek""")
+            if c == "palow":
+                d = input("do you want to buy 2 turkmen food!(yes/no)")
+                if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                    e = input("""
+                    Palow
+                    dolama
+                    manty
+                    tayborek""")
+                    if e == "Palow":
+                        print("you have to pay 50$")
+                    elif e == "Dolama":
+                        print("you have to pay 89.5$")
+                    elif e == "manty":
+                        print("you have to pay 60$")
+                    elif e == "Tayborek":
+                        print("you have to pay 60$")
+                    else:
+                        print("tazeden yazyn")
+                else:
+                    print("Sizden 40$")
+            elif c == "dolama":
+                d = input("do you want to buy 2 Turkmen food!(yes/no)")
+                if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                    e = input("""
+                    Palow
+                    dolama
+                    manty
+                    tayborek""")
+                    if e == "Palow":
+                        print("you have to pay 50$")
+                    elif e == "Dolama":
+                        print("you have to pay 89.5$")
+                    elif e == "manty":
+                        print("you have to pay 60$")
+                    elif e == "Tayborek":
+                        print("you have to pay 60$")
+                    else:
+                        print("tazeden yazyn")
+                else:
+                    print("Sizden 40$")
+            elif c == "manty":
+                d = input("do you want to buy 2 Turkmen food!(yes/no)")
+                if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                    e = input("""
+                     Palow
+                    dolama
+                    manty
+                    tayborek""")
+                    if e == "Palow":
+                        print("you have to pay 50$")
+                    elif e == "Dolama":
+                        print("you have to pay 89.5$")
+                    elif e == "manty":
+                        print("you have to pay 60$")
+                    elif e == "Tayborek":
+                        print("you have to pay 60$")
+                    else:
+                        print("tazeden yazyn")
+                else:
+                    print("sizden 29$")
+            elif c == "tayborek":
+                d = input("do you want to buy 2 Turkmen food!(yes/no)")
+                if d == "YES" or d == "yes" or d == "Yes" or d == "yES" or d == "yeS" or d == "YEs" or d == "yEs" or d == "YeS":
+                    e = input("""
+                                Palow
+                               dolama
+                               manty
+                               tayborek""")
+                    if e == "Palow":
+                        print("you have to pay 50$")
+                    elif e == "Dolama":
+                        print("you have to pay 89.5$")
+                    elif e == "manty":
+                        print("you have to pay 60$")
+                    elif e == "Tayborek":
+                        print("you have to pay 60$")
+                    else:
+                        print("tazeden yazyn")
+                else:
+                    print("Sizden 37$!")
+            else:
+                print("tazeden yazyn")
+
+else:
+    print("ok goodbye!")
